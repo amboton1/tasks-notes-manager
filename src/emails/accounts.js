e06c52149b1a570bcc7457823da2789e30a80135
amboton1 <amboton@unipu.hr>
2020-01-12 12:33:46 +0100

    Update accounts.js

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -4,17 +4,11 @@ const sendgridAPIkey = process.env.SEND_GRID_API_KEY
 
 sgMail.setApiKey(sendgridAPIkey)
 
-// sgMail.send({
-//     to: 'ammar.botonjic@gmail.com',
-//     from: 'ammar.botonjic@gmail.com',
-//     subject: 'This is my first creation',
-//     text: 'I hope this one actually get to you.'
-// })
 
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'ammar.botonjic@gmail.com',
+        from: '',
         subject: 'Do not go, please',
         text: `Dont go, mr ${name}, tell us why you canceled.`
     })
